60a0dec97fa364a7067f2120a93d523a106c953c
pranaykhadse <pranay.handysolver@gmail.com>
2026-05-06 21:11:58 +0530

    Remove all IAP payment files and references

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,16 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:lms/app/features/authentication/module/auth_module.dart';
 import 'package:lms/app/features/courses/module/courses_module.dart';
-import 'package:lms/app/features/payment/module/payment_module.dart';
 
 import 'app/features/startup/view/startup_view.dart';
 
 class AppModule extends Module {
   static const auth = "/auth";
   static const home = "/home";
-
-  /// Root route for the payment feature module.
-  static const payment = "/payment";
 
   @override
   void binds(i) {}
@@ -20,6 +16,5 @@ class AppModule extends Module {
     r.child("/", child: (context) => const StartupView());
     r.module(auth, module: AuthModule());
     r.module(home, module: CoursesModule());
-    r.module(payment, module: PaymentModule());
   }
 }
