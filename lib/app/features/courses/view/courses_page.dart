3d52f2fbaf533ab4bced651019d29c6c8e9527fc
pranaykhadse <pranay.handysolver@gmail.com>
2026-08-07 01:50:51 +0530

    Reduce Course Catalog card footer height slightly

--- lib/app/features/courses/view/courses_page.dart
+++ lib/app/features/courses/view/courses_page.dart
@@ -262,7 +262,7 @@ class _CoursesPageState extends ConsumerState<CoursesPage> {
                     crossAxisCount: columns,
                     crossAxisSpacing: width >= 760 ? 28 : 18,
                     mainAxisSpacing: width >= 760 ? 28 : 34,
-                    mainAxisExtent: width >= 760 ? 400 : 425,
+                    mainAxisExtent: width >= 760 ? 365 : 390,
                   ),
                   delegate: SliverChildBuilderDelegate(
                     (context, index) => _CatalogCourseCard(
@@ -409,7 +409,7 @@ class _CoursesPageState extends ConsumerState<CoursesPage> {
               crossAxisCount: columns,
               crossAxisSpacing: width >= 760 ? 28 : 18,
               mainAxisSpacing: width >= 760 ? 28 : 34,
-              mainAxisExtent: width >= 760 ? 400 : 425,
+              mainAxisExtent: width >= 760 ? 365 : 390,
             ),
             delegate: SliverChildBuilderDelegate(
               (context, index) => _CatalogCourseCard(
