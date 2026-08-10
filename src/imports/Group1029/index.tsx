e0cb6e9eb74aaaeba5e3f65541fc8e5837dde55c
pranaykhadse <pranay.handysolver@gmail.com>
2026-08-10 18:10:21 +0530

    Add design reference (React/Tailwind Figma components)

---
+++ src/imports/Group1029/index.tsx
@@ -0,0 +1,19 @@
+import imgScreenshot20260722At95218Pm1 from "./b3b176cfe6df2241f1ce0e89c9e4a1274bbbb703.png";
+import imgScreenshot20260722At95448Pm1 from "./045c22ef9ee047e53a45db0afc849e41db7d58ae.png";
+
+export default function Group() {
+  return (
+    <div className="contents relative size-full">
+      <div className="absolute h-[647px] left-0 top-0 w-[1440px]" data-name="Screenshot 2026-07-22 at 9.52.18 PM 1">
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <img alt="" className="absolute h-[139.1%] left-0 max-w-none top-[-39.1%] w-full" src={imgScreenshot20260722At95218Pm1} />
+        </div>
+      </div>
+      <div className="absolute h-[466px] left-0 top-0 w-[1440px]" data-name="Screenshot 2026-07-22 at 9.54.48 PM 1">
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <img alt="" className="absolute h-[193.13%] left-0 max-w-none top-[-63.73%] w-full" src={imgScreenshot20260722At95448Pm1} />
+        </div>
+      </div>
+    </div>
+  );
+}
